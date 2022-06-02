0ddf027d455e6ab08f1af56faf2eb6eed8230689
Eneko Izaguirre Martin <eneko.izaguirre@outlook.com>
2022-06-02 15:53:08 +0100

    Added a folder to get all the model that are holding data but not for DB

---
+++ BirdProject/Model/ViewModel/IndexForm.cs
@@ -0,0 +1,21 @@
+﻿namespace BirdProject.Model.ViewModel
+{
+    public class IndexForm
+    {
+        public string colourRingCode { get; set; }
+
+        public string ringColour { get; set; }
+
+        public DateOnly date { get; set; }
+
+        public string status { get; set; }
+
+        public string specie { get; set; }
+
+        public string comments { get; set; }
+
+        public float latitude { get; set; }
+
+        public float longitude { get; set; }
+    }
+}
