8bc70d8e0b362cc9742c2ee9d12734c927cbba3b
Eneko Izaguirre Martin <eneko.izaguirre@outlook.com>
2022-06-03 16:08:27 +0100

    Added the photo upload functionality

--- BirdProject/Controllers/HomeController.cs
+++ BirdProject/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
 ﻿using BirdProject.Model;
+using BirdProject.Model.ViewModel;
 using BirdProject.Models;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
@@ -19,6 +20,33 @@ namespace BirdProject.Controllers
         public IActionResult Index()
         {
             var objBird = _db.BirdBtos.ToList();
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(IndexForm form1)
+        {
+            if (ModelState.IsValid)
+            {
+                string path = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/Photos");
+
+                FileInfo fileInfo = new FileInfo(form1.birdPhoto.FileName);
+                string fileName = "F222" + fileInfo.Extension;
+
+                string fileNameWithPath = Path.Combine(path, fileName);
+
+                using (var stream = new FileStream(fileNameWithPath, FileMode.Create))
+                {
+                    form1.birdPhoto.CopyTo(stream);
+                }
+
+
+                var newSpot = new SpotLog();
+                newSpot.Longitude = form1.longitude;
+                newSpot.Latitude = form1.latitude;
+                newSpot.Date = form1.date;
+            }
+
             return View();
         }
 
