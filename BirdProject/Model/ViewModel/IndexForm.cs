e88c7d5272a12fc81497319382b7181e741061d6
Eneko Izaguirre Martin <eneko.izaguirre@outlook.com>
2022-06-03 16:09:00 +0100

    Modified the parameter to be able to receive a date and to receive a file

--- BirdProject/Model/ViewModel/IndexForm.cs
+++ BirdProject/Model/ViewModel/IndexForm.cs
@@ -1,4 +1,6 @@
-﻿namespace BirdProject.Model.ViewModel
+﻿using System.Web;
+
+namespace BirdProject.Model.ViewModel
 {
     public class IndexForm
     {
@@ -6,16 +8,18 @@
 
         public string ringColour { get; set; }
 
-        public DateOnly date { get; set; }
+        public DateTime date { get; set; }
 
         public string status { get; set; }
 
         public string specie { get; set; }
 
-        public string comments { get; set; }
+        public string? comments { get; set; }
 
         public float latitude { get; set; }
 
         public float longitude { get; set; }
+
+        public IFormFile? birdPhoto { get; set; }
     }
 }
