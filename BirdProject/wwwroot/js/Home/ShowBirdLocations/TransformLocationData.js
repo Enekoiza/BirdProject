28cb41c10386f5f46fbf8093f7a0fd45b9f39c2c
Eneko Izaguirre Martin <eneko.izaguirre@outlook.com>
2022-06-06 19:18:22 +0100

    Separated the different js for different views and controllers. Added the fetch functionality to be able to transform the grid ref to coordinates

---
+++ BirdProject/wwwroot/js/Home/ShowBirdLocations/TransformLocationData.js
@@ -0,0 +1,27 @@
+﻿var url = document.getElementById("getBirdData").value;
+
+
+async function postData(url = '', data = {}) {
+    const response = await fetch(url, {
+        method: 'post',
+        headers: {
+            'Content-Type': 'application/json',
+            'data': JSON.stringify(data)
+        }
+    });
+    return response.json();
+}
+
+
+var fetchButton = document.getElementById("fecthButton");
+var fetchInput = document.getElementById("fetchBirdInput");
+
+fetchButton.addEventListener("click", function () {
+    postData(url, { ringCode: fetchInput.value })
+        .then(data => {
+            console.log(data.birdRecords[0].longitude);
+        })
+});
+
+
+
