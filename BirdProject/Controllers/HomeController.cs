15a4b6181280909d9885b30ab76241f45b8b0d62
Eneko Izaguirre Martin <eneko.izaguirre@outlook.com>
2022-06-06 19:19:21 +0100

    Dummy data added for the fetch. Also added the upload photo functionality to avoid nulls.

--- BirdProject/Controllers/HomeController.cs
+++ BirdProject/Controllers/HomeController.cs
@@ -2,6 +2,7 @@
 using BirdProject.Model.ViewModel;
 using BirdProject.Models;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 using System.Diagnostics;
 
 namespace BirdProject.Controllers
@@ -17,9 +18,45 @@ namespace BirdProject.Controllers
             _db = db;
         }
 
+        public IActionResult ShowBirdLocations()
+        {
+
+            return View();
+        }
+
+
+        [HttpPost]
+        public async Task<string> birdData([FromHeader] string data)
+        {
+            var VM = new birdLocationsVM();
+            List<birdRecordVM> birdRecords = new List<birdRecordVM>();
+
+
+            var bird1 = new birdRecordVM { longitude = 25.2F, latitude = 25.2F, gridRef = null };
+            var bird2 = new birdRecordVM { longitude = 40.2F, latitude = 25.2F, gridRef = null };
+            var bird3 = new birdRecordVM { longitude = 23.4F, latitude = 25.2F, gridRef = null };
+            var bird4 = new birdRecordVM { longitude = null, latitude = null, gridRef = "ST2264" };
+
+
+
+            birdRecords.Add(bird1);
+            birdRecords.Add(bird2);
+            birdRecords.Add(bird3);
+            birdRecords.Add(bird4);
+
+            VM.birdRecords = birdRecords;
+
+
+
+
+            string a = JsonConvert.SerializeObject(VM);
+            return a;
+
+        }
+
         public IActionResult Index()
         {
-            var objBird = _db.BirdBtos.ToList();
+
             return View();
         }
 
@@ -28,17 +65,25 @@ namespace BirdProject.Controllers
         {
             if (ModelState.IsValid)
             {
-                string path = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/Photos");
-
-                FileInfo fileInfo = new FileInfo(form1.birdPhoto.FileName);
-                string fileName = "F222" + fileInfo.Extension;
-
-                string fileNameWithPath = Path.Combine(path, fileName);
-
-                using (var stream = new FileStream(fileNameWithPath, FileMode.Create))
+                if(form1.birdPhoto != null)
                 {
-                    form1.birdPhoto.CopyTo(stream);
+                    string path = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/Photos");
+
+                    string specie = form1.specie;
+                    string ringCode = form1.colourRingCode;
+
+                    FileInfo fileInfo = new FileInfo(form1.birdPhoto.FileName);
+                    string fileName = "F22" + fileInfo.Extension;
+
+                    string fileNameWithPath = Path.Combine(path, fileName);
+
+                    using (var stream = new FileStream(fileNameWithPath, FileMode.Create))
+                    {
+                        form1.birdPhoto.CopyTo(stream);
+                    }
                 }
+
+                
 
 
                 var newSpot = new SpotLog();
