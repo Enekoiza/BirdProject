e93228c252917473860a96a1663a4c2cea59a9b2
Eneko Izaguirre <eneko.izaguirre@outlook.com>
2022-06-29 23:30:54 +0100

    New view model to hold the bird coordinates but also add the metal ring requested by the user

---
+++ BirdProject/Model/ViewModel/birdDataSolutionVM.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Newtonsoft.Json;
+
+namespace BirdProject.Model.ViewModel
+{
+	public class birdDataSolutionVM
+	{
+        [JsonProperty("metalRingID")]
+        public string metalRingID;
+
+        [JsonProperty("birdData")]
+        public List<birdRecordVM> birdData;
+    }
+}
+
