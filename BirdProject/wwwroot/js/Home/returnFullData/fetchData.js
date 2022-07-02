72ed5585e1b780109f0569e308631147c401a2e1
Eneko Izaguirre <eneko.izaguirre@outlook.com>
2022-07-02 09:57:51 +0100

    Js file to fecth the date from db and convert the gridref if necessary. Then redirect to the next controller method.

---
+++ BirdProject/wwwroot/js/Home/returnFullData/fetchData.js
@@ -0,0 +1,55 @@
+﻿import { getLat, getLng } from "../../modules/GeoTools.js";
+
+
+
+
+var url = document.getElementById("getBirdData").value;
+
+console.log(url);
+
+async function postData(url = '', data = {}) {
+    const response = await fetch(url, {
+        method: 'post',
+        headers: {
+            'Content-Type': 'application/json',
+            'data': JSON.stringify(data)
+        }
+    });
+    return response.json();
+}
+
+
+var fetchInput = document.getElementById("metalRing");
+
+console.log(fetchInput.value);
+
+window.addEventListener("DOMContentLoaded", function () {
+    postData(url, { ringCode: fetchInput.value })
+        .then(data => {
+
+
+
+            for (let i = 0; i < data.birdData.length; i++) {
+                if (data.birdData[i].gridRef != null) {
+                    data.birdData[i].latitude = getLat(data.birdData[i].gridRef);
+                    data.birdData[i].longitude = getLng(data.birdData[i].gridRef);
+                }
+
+
+            }
+            console.log(data.birdData);
+            console.log(data);
+
+            var url1 = "/results/generateSearchResult";
+
+
+            var asd = url1 + "?test=" + JSON.stringify(data);
+
+            console.log(JSON.stringify(data));
+
+            console.log(asd);
+
+            window.location = asd;
+
+        })
+});
