c1c820778a7ce684085b9c886b2564a2834c6e55
Eneko Izaguirre <eneko.izaguirre@outlook.com>
2022-07-02 09:54:28 +0100

    Removed jsonproperties

--- BirdProject/Model/ViewModel/birdDataSolutionVM.cs
+++ BirdProject/Model/ViewModel/birdDataSolutionVM.cs
@@ -5,10 +5,8 @@ namespace BirdProject.Model.ViewModel
 {
 	public class birdDataSolutionVM
 	{
-        [JsonProperty("metalRingID")]
         public string metalRingID;
 
-        [JsonProperty("birdData")]
         public List<birdRecordVM> birdData;
     }
 }
