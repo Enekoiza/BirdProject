08e620c8a14be7d2e7db7ad967d3f867d4736040
Eneko Izaguirre <eneko.izaguirre@outlook.com>
2022-07-02 10:03:30 +0100

    Generate the longitude and latitude from a given grid ref and return that data to the backend through a JSON string.

--- BirdProject/wwwroot/js/Home/ShowBirdLocations/TransformLocationData.js
+++ BirdProject/wwwroot/js/Home/ShowBirdLocations/TransformLocationData.js
@@ -6,6 +6,7 @@
 
 var url = document.getElementById("getBirdData").value;
 
+console.log(url);
 
 async function postData(url = '', data = {}) {
     const response = await fetch(url, {
@@ -25,9 +26,30 @@ var fetchInput = document.getElementById("fetchBirdInput");
 fetchButton.addEventListener("click", function () {
     postData(url, { ringCode: fetchInput.value })
         .then(data => {
-            console.log(data.birdRecords[0].longitude);
+
+            
+
+            for (let i = 0; i < data.birdData.length; i++) {
+                if (data.birdData[i].gridRef != null) {
+                    data.birdData[i].latitude = getLat(data.birdData[i].gridRef);
+                    data.birdData[i].longitude = getLng(data.birdData[i].gridRef);
+                }
+
+
+            } 
+            console.log(data.birdData);
+            console.log(data);
+
+            var url1 = "/results/generateSearchResult";
+
+
+            var asd = url1 + "?test=" + JSON.stringify(data);
+
+            console.log(JSON.stringify(data));
+
+            console.log(asd);
+
+            window.location = asd;
+
         })
 });
-
-
-
