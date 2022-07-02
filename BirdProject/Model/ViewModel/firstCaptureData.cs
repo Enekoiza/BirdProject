36cf04c08071b30d6ba9ce2b68de39f12631da8e
Eneko Izaguirre <eneko.izaguirre@outlook.com>
2022-07-02 10:04:16 +0100

    View model that holds the data about the first capture which is hold in the BTO table.

---
+++ BirdProject/Model/ViewModel/firstCaptureData.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace BirdProject.Model.ViewModel
+{
+	public class firstCaptureData
+	{
+        public string Sex { get; set; } = null!;
+        public string Specie { get; set; } = null!;
+        public string? ColourRingCode { get; set; }
+        public string? ColourRingColour { get; set; }
+        public string? ColourRingPosition { get; set; }
+    }
+}
+
