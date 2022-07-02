310a99aa771da973813e82df5ce71456566e1416
Eneko Izaguirre <eneko.izaguirre@outlook.com>
2022-07-02 10:02:12 +0100

    View model to hold the data about the first capture, the rest captures and the citynames regarding those captures.

---
+++ BirdProject/Model/ViewModel/fullBirdData.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace BirdProject.Model.ViewModel
+{
+	public class fullBirdData
+	{
+        public firstCaptureData firstCapture;
+
+        public birdDataSolutionVM restCaptures;
+
+        public List<string> cityNames { get; set; }
+    }
+}
+
